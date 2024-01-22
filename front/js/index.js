77c3539039646f2547b6eb904629e87410fb32ac
Léo Mendes <leo.mendes-melin@hotmail.fr>
2024-01-22 18:00:05 +0100

    adding base code server working Ref #6

--- front/js/index.js
+++ front/js/index.js
@@ -1,11 +1,31 @@
 import { GamePresenter } from './Game/GamePresenter.js';
-import { GameModel } from './Game/GameModel.js';
+import { GameModel } from '../../back/logic/Model/Game/GameModel.js';
 import { GameView } from './Game/GameView.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     const model = new GameModel();
     const view = new GameView();
     const presenter = new GamePresenter(model, view);
+
+
+    const token = localStorage.getItem('token');
+
+    const socket = io('http://localhost:3000', {
+        auth: {
+            token: token
+        }
+    });
+
+    socket.on('connect', () => {
+        console.log('Connected to the server');
+        // Démarrer le jeu ou autre logique ici
+        socket.emit('start game', { /* options de jeu */ });
+    });
+
+    socket.on('connect_error', (err) => {
+        console.log('Connection failed', err.message);
+    });
+
 
 
 });
