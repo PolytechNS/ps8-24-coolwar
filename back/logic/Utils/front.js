f96a2f692ef04b610ea6d6ee9ce0f20cc7880667
ClervieCauser <clervie.causer@etu.unice.fr>
2024-02-05 09:49:29 +0100

    naming refactor

--- back/logic/Utils/front.js
+++ back/logic/Utils/front.js
@@ -9,7 +9,7 @@ const path = require('path');
 // Note that fs methods consider the current folder to be the one where the app is run, that's why we don't need the "../.." before front.
 const baseFrontPath = '/front';
 // If the user requests a directory, a file can be returned by default.
-const defaultFileIfFolder = "index.html";
+const defaultFileIfFolder = "GamePage.html";
 
 /* Dict associating files' extension to a MIME type browsers understand. The reason why this is needed is that only
 ** the file's content is sent to the browser, so it cannot know for sure what kind of file it was to begin with,
