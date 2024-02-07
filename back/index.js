831257a7e8cb1ac9808ffa5bbcd840cff0b8c6cb
Léo Mendes <leo.mendes-melin@hotmail.fr>
2024-02-07 19:38:04 +0100

    adding docker part for mongo db Ref #14

--- back/index.js
+++ back/index.js
@@ -1,17 +1,31 @@
-// server.js
 const http = require('http');
+const { MongoClient } = require('mongodb');
 const setupSocketServer = require('./sockerServer');
 const mainRouter = require('./logic/Router/MainRouter');
-const { PORT } = require('./logic/Utils/constants');
+const { PORT, MONGO_URL } = require('./logic/Utils/constants'); // Assurez-vous d'ajouter MONGO_URL dans vos constants
 
-const server = http.createServer((req, res) => {
-    // Delegate the request to the mainRouter
-    mainRouter.handle(req, res);
-    console.log("j'attends les requetes");
-});
+const client = new MongoClient(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-setupSocketServer(server);
+async function startServer() {
+    try {
+        await client.connect();
+        console.log('Connecté à la base de données MongoDB');
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+        const db = client.db(); // Ajoutez la logique nécessaire pour utiliser 'db'
+        const server = http.createServer((req, res) => {
+            // Vous pouvez passer 'db' à mainRouter si nécessaire
+            mainRouter.handle(req, res);
+            console.log("j'attends les requetes");
+        });
+
+        setupSocketServer(server);
+
+        server.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (err) {
+        console.error('Erreur de connexion à MongoDB', err);
+    }
+}
+
+startServer();
--- back/logic/Utils/constants.js
+++ back/logic/Utils/constants.js
@@ -1,5 +1,6 @@
 // constants.js
 module.exports = {
     JWT_SECRET_KEY: 'secret_key',
-    PORT: 3000
+    PORT: 3000,
+    MONGO_URL: 'mongodb://mongodb:27017/myappdb'
 };
