36838e83be11f16a8a6ee04862e8711b36cc8e05
Léo Mendes <leo.mendes-melin@hotmail.fr>
2024-03-10 12:55:29 +0100

    refactoring back-end / front end for new features

--- front/js/Game/Models/Index/GameIndexWithBot.js
+++ front/js/Game/Models/Index/GameIndexWithBot.js
@@ -1,25 +1,22 @@
 // GameIndexWithBot.js
-import {GamePresenter} from './GamePresenter.js';
+import {GamePresenter} from './Controllers/GamePresenter.js';
 import {actionGameService} from "../Services/actionGameService.js";
-import {gameService} from "../Services/gameService.js";
 import {GameView} from './GameView.js';
-import {socketManager} from '../socket/socketManager.js';
+import {socketManager} from '../Socket/socketManager.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     // Assurez-vous que le token est disponible
-    console.log("load");
     const token = localStorage.getItem('token');
     if (token) {
         socketManager.initializeSocket(token);
-
 
         // Vérifiez que la socket est prête avant de lancer le jeu
         const checkSocketReady = setInterval(() => {
             if (socketManager.isSocketInitialized()) {
                 clearInterval(checkSocketReady);
-                // La socket est prête, initialisons le jeu
-                gameService.getGameWithIdUser((serializedGameModel) => {
 
+                // La socket est prête, initialisons le jeu
+                actionGameService.getGameWithBot((serializedGameModel) => {
                     // Désérialisez le modèle de jeu JSON en objet JavaScript
                     const model = JSON.parse(serializedGameModel); // Assurez-vous que ce modèle est correctement formaté
                     const view = new GameView(model);
