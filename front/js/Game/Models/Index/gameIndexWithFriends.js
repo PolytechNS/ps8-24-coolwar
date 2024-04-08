03cae431cc9fdfd7c3dc35c58a5cc559738fe153
ClervieCauser <clervie.causer@etu.unice.fr>
2024-04-08 19:03:04 +0200

    css insert son de leo

--- front/js/Game/Models/Index/gameIndexWithFriends.js
+++ front/js/Game/Models/Index/gameIndexWithFriends.js
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             attachAchievementsListener();
         }
-
+        
         userService.getUserInfo((userInfo) => {
             console.log("PROFILE USER info", userInfo);
             if (userInfo.achievements && userInfo.achievements.includes("tu_fais_le_fou.jpg")) {
@@ -172,10 +172,10 @@ function insertSoundCloudPlayer() {
     const soundCloudContainer = document.getElementById('soundcloud-container');
     if (true) {
         const playerHtml = `
-            <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1788018703%3Fsecret_token%3Ds-uqqvuwf8xj5&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-            <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
-                <a href="https://soundcloud.com/leomendesmusic" title="leomendesmusic" target="_blank" style="color: #cccccc; text-decoration: none;">leomendesmusic</a> · 
-                <a href="https://soundcloud.com/leomendesmusic/drip-man-tu-fais-le-fou-vella-ps8/s-uqqvuwf8xj5" title="Drip Man - Tu fais le fou (Vella PS8)" target="_blank" style="color: #cccccc; text-decoration: none;">Drip Man - Tu fais le fou (Vella PS8)</a>
+            <iframe width="90%" height="90%" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1788018703%3Fsecret_token%3Ds-uqqvuwf8xj5&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" style="margin: 0;"></iframe>
+            <div style="font-size: 10px; color: #cccccc;padding-left: 30px; padding-right: 30px;margin:0;">
+                <a href="https://soundcloud.com/leomendesmusic" title="leomendesmusic" target="_blank" style="color: #cccccc; text-decoration: none;margin:0;">leomendesmusic</a> · 
+                <a href="https://soundcloud.com/leomendesmusic/drip-man-tu-fais-le-fou-vella-ps8/s-uqqvuwf8xj5" title="Drip Man - Tu fais le fou (Vella PS8)" target="_blank" style="color: #cccccc; text-decoration: none;margin:0;">Drip Man - Tu fais le fou (Vella PS8)</a>
             </div>
         `;
         soundCloudContainer.innerHTML = playerHtml;
