47724186061312f58c1a20a2636e0fe7a447461d
Julien D. <juliendidier83550@gmail.com>
2024-04-08 23:05:20 +0200

    changing adresses

--- front/js/Utils/config.js
+++ front/js/Utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_ENDPOINT: 'http://localhost:3000',
+    API_ENDPOINT: 'http://ec2-15-188-69-101.eu-west-3.compute.amazonaws.com:3000',
     withBot : 'withBot',
     withFriends : 'withFriends',
     offline : 'offline',
