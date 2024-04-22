df04abc87f5ce36590c022db3a8405ecb2642084
Julien D. <juliendidier83550@gmail.com>
2024-04-22 11:20:38 +0200

    modifying CORS rules

--- front/js/Utils/config.js
+++ front/js/Utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_ENDPOINT: 'http://ec2-15-188-69-101.eu-west-3.compute.amazonaws.com:3000',
+    API_ENDPOINT: 'http://coolwar.ps8.academy/',
     withBot : 'withBot',
     withFriends : 'withFriends',
     offline : 'offline',
