d03115d21f22201d86f34ec75235f6520a6b0bd5
ClervieCauser <clervie.causer@etu.unice.fr>
2024-04-25 19:37:34 +0200

    hallowwwwwwwweeen background

--- coolWarApp/www/js/Services/Event/eventHandler.js
+++ coolWarApp/www/js/Services/Event/eventHandler.js
@@ -2,14 +2,11 @@ import {socketManager} from "../../Socket/socketManager.js";
 
 
 document.addEventListener('DOMContentLoaded', () => {
-
-
-
     socketManager.socket.on('event alert', (data) => {
         const eventAlertSpan = document.querySelector('.alert');
         const wootDance = document.querySelector('.woot-dance');
         const ghostCanvas = document.querySelector('#ghost');
-
+        const tank = document.querySelector('.tank');
 
         console.log('event alert', data);
 
@@ -22,8 +19,110 @@ document.addEventListener('DOMContentLoaded', () => {
                     console.log('alert', eventAlertSpan);
                     eventAlertSpan.textContent = "Achievement débloqué";
                     ghostCanvas.style.display = 'block';
-                    wootDance.style.display = 'none';
+                    document.body.insertAdjacentHTML('afterbegin', `
+                    <div class="container ">
+                        <!-- Background -->
+                        <div class="moon"></div>
+                        <div class="clouds cloud1">
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div class="clouds cloud2">
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div class="clouds cloud3">
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div class="clouds cloud4">
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div class="clouds cloud5">
+                            <div></div>
+                            <div></div>
+                        </div>
+                    
+                        <div class="smoke">
+                            <div></div>
+                        </div>
+                    
+                        <!-- Lantern Garland -->
+                        <div class="dancing-line">
+                    
+                            <div class="pumpkin">
+                                <div class="stem"></div>
+                                <div class="heart"></div>
+                                <div class="rounded-eyes"></div>
+                                <div class="rounded-eyes"></div>
+                                <div class="mean-mouth"></div>
+                            </div>
+                    
+                            <div class="pumpkin">
+                                <div class="stem"></div>
+                                <div class="heart"></div>
+                                <div class="eye"></div>
+                                <div class="eye eye-right"></div>
+                                <div class="bb-mouth"></div>
+                            </div>
+                    
+                            <div class="pumpkin">
+                                <div class="stem"></div>
+                                <div class="heart"></div>
+                                <div class="rounded-eyes baby-eyes"></div>
+                                <div class="rounded-eyes baby-eyes"></div>
+                                <div class="mean-mouth"></div>
+                            </div>
+                    
+                            <div class="pumpkin">
+                                <div class="stem"></div>
+                                <div class="heart"></div>
+                                <div class="rounded-eyes"></div>
+                                <div class="rounded-eyes"></div>
+                                <div class="rounded-mouth"></div>
+                            </div>
+                    
+                            <div class="pumpkin">
+                                <div class="stem"></div>
+                                <div class="heart"></div>
+                                <div class="eye"></div>
+                                <div class="eye eye-right"></div>
+                                <div class="bb-mouth"></div>
+                            </div>
+                    
+                            <div class="pumpkin">
+                                <div class="stem"></div>
+                                <div class="heart"></div>
+                                <div class="rounded-eyes"></div>
+                                <div class="rounded-eyes"></div>
+                                <div class="mean-mouth"></div>
+                            </div>
+                    
+                            <div class="pumpkin">
+                                <div class="stem"></div>
+                                <div class="heart"></div>
+                                <div class="eye"></div>
+                                <div class="eye eye-right"></div>
+                                <div class="bb-mouth"></div>
+                            </div>
+                    
+                            <div class="pumpkin">
+                                <div class="stem"></div>
+                                <div class="heart"></div>
+                                <div class="rounded-eyes baby-eyes"></div>
+                                <div class="rounded-eyes baby-eyes"></div>
+                                <div class="mean-mouth"></div>
+                            </div>
+                    
+                        </div>
+                    </div>`);
 
+                    let body = document.querySelector('body');
+                    body.style.background='radial-gradient(ellipse at center, #160909 0%, #05060f 45%, #0d0b30 100%)';
+
+                    wootDance.style.display = 'none';
+                    tank.style.display = 'none';
                     // Set another timeout to hide the alert after 5 seconds
                     setTimeout(() => {
                         eventAlertSpan.style.display = 'none';
--- coolWarApp/www/js/User/userInfo.js
+++ coolWarApp/www/js/User/userInfo.js
@@ -216,5 +216,3 @@ function listenToNotifications() {
 
 // Appeler cette fonction pour injecter les styles lorsque le script est exécuté
 injectNotificationStyles();
-
-
